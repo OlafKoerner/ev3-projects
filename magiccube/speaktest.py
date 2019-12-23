7fb68420917894e073d36a4b38bdd8206ba9e44c
OlafKoerner <59175692+OlafKoerner@users.noreply.github.com>
2019-12-23 19:09:35 +0100

    Rename magiccube/speaktest.py to others/speaktest.py

--- magiccube/speaktest.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-import ev3dev.ev3 as ev3
-
-ev3.Sound.speak('Hello Aiko mate! How do you do? Had a great weekend?').wait()
---
+++ others/speaktest.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+import ev3dev.ev3 as ev3
+
+ev3.Sound.speak('Hello Aiko mate! How do you do? Had a great weekend?').wait()
