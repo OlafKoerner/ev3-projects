14b41644b4d6c19eb6e775f65719dc4565923d0e
FenjaKoerner <59176180+FenjaKoerner@users.noreply.github.com>
2019-12-27 18:36:44 +0100

    re-organized to allow working in parallel

--- magiccube/magiccube_device.py
+++ magiccube/magiccube_device.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 import ev3dev.ev3 as ev3
+from time import sleep
 
 #  Connect motors on output ports
 motor_a = ev3.Motor(ev3.OUTPUT_A)
 assert motor_a.connected
+
+motor_b = ev3.Motor(ev3.OUTPUT_B)
+assert motor_b.connected
+
+btn = ev3.Button()
 
 # Connect color sensors
 #cs = ColorSensor()
@@ -34,7 +40,20 @@ btn = ev3.Button()
 # lower case letter means counter clockwise turn
 
 print('Motor start turning...')
-motor_a.run_to_rel_pos(position_sp=1000, speed_sp=100)
-print('... motor stopped turning.')
+motor_a.run_to_rel_pos(position_sp=300, speed_sp=200)
+while motor_a.is_running: sleep(0.1)
+sleep(1)
+
+motor_a.run_to_rel_pos(position_sp=-30, speed_sp=200)
+while motor_a.is_running: sleep(0.1)
+sleep(1)
+
+motor_b.run_to_rel_pos(position_sp=120, speed_sp=200)
+while motor_b.is_running: sleep(0.1)
+sleep(1)
+
+motor_b.run_to_rel_pos(position_sp=-120, speed_sp=150)
+while motor_b.is_running: sleep(0.1)
+sleep(1)
 
 ev3.Sound.speak('Cube is solved!').wait()
