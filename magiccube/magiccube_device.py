209ca780ba6b348d7c4c9b7d45f681e2d5ccacac
FenjaKoerner <59176180+FenjaKoerner@users.noreply.github.com>
2020-04-18 13:29:42 +0200

    introduced TkAgg to be platform independent

--- magiccube/magiccube_device.py
+++ magiccube/magiccube_device.py
@@ -3,57 +3,58 @@
 import ev3dev.ev3 as ev3
 from time import sleep
 
-#  Connect motors on output ports
-motor_a = ev3.Motor(ev3.OUTPUT_A)
-assert motor_a.connected
+def main(p1, p2, p3, p4):
+    #  Connect motors on output ports
+    motor_a = ev3.Motor(ev3.OUTPUT_A)
+    assert motor_a.connected
 
-motor_b = ev3.Motor(ev3.OUTPUT_B)
-assert motor_b.connected
+    motor_b = ev3.Motor(ev3.OUTPUT_B)
+    assert motor_b.connected
 
-btn = ev3.Button()
+    btn = ev3.Button()
 
-# Connect color sensors
-#cs = ColorSensor()
-#assert cs.connected
+    # Connect color sensors
+    #cs = ColorSensor()
+    #assert cs.connected
 
-fNoColor = 0
-fBlack = 1
-fBlue = 2
-fGreen = 3
-fYellow = 4
-fRed = 5
-fWhite = 6
-fBrown = 7
+    fNoColor = 0
+    fBlack = 1
+    fBlue = 2
+    fGreen = 3
+    fYellow = 4
+    fRed = 5
+    fWhite = 6
+    fBrown = 7
 
-print('Robot Starting')
-ev3.Sound.speak('Okay folks... Let us solve the cube!').wait()
+    print('Robot Starting')
+    ev3.Sound.speak('Okay folks... Let us solve the cube!').wait()
 
-# We will need to check EV3 buttons state
-btn = ev3.Button()
+    # We will need to check EV3 buttons state
+    btn = ev3.Button()
 
-# 'R' turn right side clockwise
-# 'L' turn left side clockwise
-# 'F' turn front side clockwise
-# 'B' turn back side clockwise
-# 'U' turn up side clockwise
-# 'D' turn down clockwise
-# lower case letter means counter clockwise turn
+    # 'R' turn right side clockwise
+    # 'L' turn left side clockwise
+    # 'F' turn front side clockwise
+    # 'B' turn back side clockwise
+    # 'U' turn up side clockwise
+    # 'D' turn down clockwise
+    # lower case letter means counter clockwise turn
 
-print('Motor start turning...')
-motor_a.run_to_rel_pos(position_sp=300, speed_sp=200)
-while motor_a.is_running: sleep(0.1)
-sleep(1)
+    print('Motor start turning...')
+    motor_a.run_to_rel_pos(position_sp=p1, speed_sp=200)
+    while motor_a.is_running: sleep(0.1)
+    sleep(1)
 
-motor_a.run_to_rel_pos(position_sp=-30, speed_sp=200)
-while motor_a.is_running: sleep(0.1)
-sleep(1)
+    motor_a.run_to_rel_pos(position_sp=p2, speed_sp=200)
+    while motor_a.is_running: sleep(0.1)
+    sleep(1)
 
-motor_b.run_to_rel_pos(position_sp=120, speed_sp=200)
-while motor_b.is_running: sleep(0.1)
-sleep(1)
+    motor_b.run_to_rel_pos(position_sp=p3, speed_sp=200)
+    while motor_b.is_running: sleep(0.1)
+    sleep(1)
 
-motor_b.run_to_rel_pos(position_sp=-120, speed_sp=150)
-while motor_b.is_running: sleep(0.1)
-sleep(1)
+    motor_b.run_to_rel_pos(position_sp=p4, speed_sp=150)
+    while motor_b.is_running: sleep(0.1)
+    sleep(1)
 
-ev3.Sound.speak('Cube is solved!').wait()
+    ev3.Sound.speak('Cube is solved!').wait()
--- magiccube/magiccube_main.py
+++ magiccube/magiccube_main.py
@@ -1,6 +1,8 @@
 #import magiccube_solver as mcs
 import magiccube_device as mcd
+import sys
 
 if __name__ == "__main__":
     #mcs.main()
-    mcd.main()
+    args = sys.argv
+    mcd.main(args[1], args[2], args[3], args[4])
